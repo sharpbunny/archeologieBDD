e06b7356c1b89ed71ed146e38a45be62263590fc
Chris.D <fusion101box@gmail.com>
2017-03-20 14:49:02 +0100

    Add link with database

---
+++ tp10/departement.cs
@@ -0,0 +1,28 @@
+namespace tp10
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("departement")]
+    public partial class departement
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public departement()
+        {
+            Communes = new HashSet<Commune>();
+        }
+
+        [Key]
+        public int ID_departement { get; set; }
+
+        [Required]
+        [StringLength(40)]
+        public string nom { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Commune> Communes { get; set; }
+    }
+}
---
+++ tp10/site_intervention.cs
@@ -0,0 +1,48 @@
+namespace tp10
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    public partial class site_intervention
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public site_intervention()
+        {
+            interventions = new HashSet<intervention>();
+            periodes = new HashSet<periode>();
+            themes = new HashSet<theme>();
+            type_intervention = new HashSet<type_intervention>();
+        }
+
+        [Key]
+        [StringLength(40)]
+        public string ID_site { get; set; }
+
+        [Required]
+        [StringLength(60)]
+        public string nom_site { get; set; }
+
+        public float latitude { get; set; }
+
+        public float longitude { get; set; }
+
+        public int ID_commune { get; set; }
+
+        public virtual Commune Commune { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<intervention> interventions { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<periode> periodes { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<theme> themes { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<type_intervention> type_intervention { get; set; }
+    }
+}
