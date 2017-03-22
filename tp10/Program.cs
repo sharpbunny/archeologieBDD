5f196c244cad1e7590b037cc5a00e603e8642a2c
waldodev <hinakoshijou@outlook.fr>
2017-03-22 09:05:34 +0100

    program.cs modifier

--- tp10/Program.cs
+++ tp10/Program.cs
@@ -10,9 +10,18 @@ namespace tp10
 	{
 		private static JsonFile fichierJson;
 		private static string _fichierJson= "C:/Users/34011-14-02/Documents/waldodevgitbash/archeologieBDD/tp10/bin/Debug/listIntervention.json";
-		
-		private List<JsonFile> collection=new List<JsonFile>();
-		
+
+		private Dictionary<int,string> datasetid = new Dictionary<int,string>();
+		private string[] recordid;
+		private string[] fields;
+		private double[] fieldscoordonnee_wgs84;
+		private int[] fieldsdate;
+		private string[] geometry;
+		private double[] geometrycoordinates;
+		private double[] record_timestamp;
+
+		//private static List<JsonFile> collection=new List<JsonFile>();
+
 		/// <summary>
 		/// Affichage de l'aide du programme en cas d'entrée erronée.
 		/// </summary>
@@ -57,12 +66,20 @@ namespace tp10
 			}
 									
 			foreach (var item in fichierJson.TableauJson)
-			{
-				//Console.WriteLine("{0} {1} {2} {3} {4} {5} {6} {7} {8} {9} {10} {11} {12} {13}", item.datasetid, item.recordid, item.fields.departement, item.fields.commune, item.fields.periode_s, item.fields.coordonnee_wgs84, item.fields.nom_du_site, item.fields.date_fin.getType(), item.fields.type_d_intervention, item.fields.date_debut.ToString.getType(), item.geometry.type, item.geometry.coordinates);
+			{			
+				//Console.WriteLine("{0} {1} {2} {3} {4} {5} {6} {7} {8} {9} {10} {11} {12} {13}", item.datasetid, item.recordid, item.fields.departement, item.fields.commune, item.fields.periode_s, item.fields.coordonnee_wgs84, item.fields.nom_du_site, item.fields.date_fin, item.fields.type_d_intervention, item.fields.date_debut, item.geometry.type, item.geometry.coordinates);
 				//Console.WriteLine("{0} {1} {2} {3} {4} {5} {6} {7} {8} {9} ", item.datasetid, item.recordid, item.fields.departement, item.fields.commune, item.fields.periode_s, item.fields.coordonnee_wgs84, item.fields.nom_du_site, item.fields.type_d_intervention, item.geometry.type, item.geometry.coordinates);
-				Console.WriteLine(item.datasetid +" | "+ item.recordid + " | " + item.fields.departement + " | " + item.fields.commune + " | " + item.fields.periode_s + " | " + item.fields.coordonnee_wgs84 + " | " + item.fields.nom_du_site + " | " + item.fields.type_d_intervention + " | " + item.geometry.type + " | " + item.geometry.coordinates);				
+				Console.WriteLine(item.datasetid +" | "+ item.recordid + " | " + item.fields.departement + " | " + item.fields.commune + " | " + item.fields.periode_s + " | " + item.fields.coordonnee_wgs84 + " | " + item.fields.nom_du_site + " | " + item.fields.type_d_intervention + " | " + item.geometry.type + " | " + item.geometry.coordinates);	
 			}
+
+		
 		}
+
+
+
+
+
+
 
 
 	}
