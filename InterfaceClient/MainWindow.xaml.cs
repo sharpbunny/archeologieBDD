0aaee2cdd4e6eeb286a1f99dae63e4dcd302733e
FRANCE Frédéric <frederic.france@free.fr>
2017-03-23 22:36:03 +0100

    config for database

--- InterfaceClient/MainWindow.xaml.cs
+++ InterfaceClient/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using tp10;
+using System.Configuration;
 
 namespace InterfaceClient
 {
@@ -24,52 +26,77 @@ namespace InterfaceClient
 		{
 			InitializeComponent();
 
-            affichageDonnees.ItemsSource = ChargmentDonnees();
+			affichageDonnees.ItemsSource = ChargementDonnees();
 
 		}
 
-        private void triCommune_Click(object sender, RoutedEventArgs e)
-        {
-            
+		private void triCommune_Click(object sender, RoutedEventArgs e)
+		{
 
 
-        }
 
-        /// <summary>
-        /// Permet de charger les données des sites d'interventions depuis la BDD.
-        /// </summary>
-        /// <returns>Liste des interventions</returns>
-        private List<tp10.site_intervention>ChargmentDonnees()
-        {
-            List<tp10.site_intervention> siteInterventions = new List<tp10.site_intervention>();
-            using (tp10.archeoContext contextSiteIntervention = new tp10.archeoContext())
-            {
-                var seeAll = from u in contextSiteIntervention.site_intervention
-                             select u;
-                foreach(var item in seeAll)
-                {
-                    siteInterventions.Add(new tp10.site_intervention() {
+		}
 
-                        ID_site = "hidden",
+		/// <summary>
+		/// Permet de charger les données des sites d'interventions depuis la BDD.
+		/// </summary>
+		/// <returns>Liste des interventions</returns>
+		private List<site_intervention> ChargementDonnees()
+		{
+			List<site_intervention> siteInterventions = new List<site_intervention>();
+			using (archeoContext contextSiteIntervention = new archeoContext())
+			{
+				try
+				{
+					var seeAll = from u in contextSiteIntervention.site_intervention
+								 select u;
+					foreach (var item in seeAll)
+					{
+						siteInterventions.Add(new tp10.site_intervention()
+						{
 
-                        nom_site = item.nom_site,
+							ID_site = "hidden",
 
-                        periodes = item.periodes,
+							nom_site = item.nom_site,
 
-                        themes = item.themes,
+							periodes = item.periodes,
 
-                        latitude = item.latitude,
+							themes = item.themes,
 
-                        longitude = item.longitude
-                        });
-                }
+							latitude = item.latitude,
 
-                return siteInterventions;
+							longitude = item.longitude
+						});
+					}
 
-            }
-            
-            
-        }
+				}
+				catch (Exception err)
+				{
+					MessageBox.Show(ConfigurationManager.ConnectionStrings["archeoContext"].ToString() + "\nLa connexion n'est pas valide" + err.Message, "Serveur/Database Incorrect");
+				}
+				return siteInterventions;
+			}
+		}
 
-    }
+		/// <summary>
+		/// Sortie de l'application.
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void MenuItem_Click(object sender, RoutedEventArgs e)
+		{
+			Application.Current.Shutdown();
+		}
+
+		/// <summary>
+		/// Paramètres de connexion à la base de données.
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void MenuParametres_Click(object sender, RoutedEventArgs e)
+		{
+			Parametres param = new Parametres();
+			param.ShowDialog();
+		}
+	}
 }
---
+++ InterfaceClient/Parametres.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Xml;
+using System.Diagnostics;
+using System.Data;
+
+namespace InterfaceClient
+{
+	/// <summary>
+	/// Logique d'interaction pour Parametres.xaml
+	/// </summary>
+	public partial class Parametres : Window
+	{
+		public Parametres()
+		{
+			InitializeComponent();
+		}
+
+		private void buttonConnect_Click(object sender, RoutedEventArgs e)
+		{
+			try
+			{
+				// On construit le connexion string
+				StringBuilder Connect = new StringBuilder("data Source=");
+				Connect.Append(textServer.Text);
+				Connect.Append(";initial Catalog=");
+				Connect.Append(textDatabase.Text);
+				Connect.Append(";integrated security=True;MultipleActiveResultSets=True;App=EntityFramework");
+				string strCon = Connect.ToString();
+				Debug.WriteLine(strCon);
+				updateConfigFile(strCon);
+				// On crée une nouvelle connexion.
+				SqlConnection Db = new SqlConnection();
+				// On rafraîchit sinon il utilise la valeur précédente.
+				ConfigurationManager.RefreshSection("connectionStrings");
+				Db.ConnectionString = ConfigurationManager.ConnectionStrings["archeoContext"].ToString();
+				// On lance une requête pour vérifier que le connexion string fonctionne.
+				SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM dbo.Commune", Db);
+				DataTable dt = new DataTable();
+				da.Fill(dt);
+				//comboBox.DataSource = dt;
+				comboBox.Items.Add("azerty");
+				comboBox.SelectedIndex = 0;
+				//comboBox.DisplayMember = "nom";
+			}
+			catch (Exception err)
+			{
+				MessageBox.Show(ConfigurationManager.ConnectionStrings["archeoContext"].ToString() + ".La connexion n'est pas valide\n" + err.Message, "Serveur/Database Incorrect");
+			}
+		}
+
+		/// <summary>
+		/// Mise à jour fichier de configuration.
+		/// </summary>
+		/// <param name="conf"></param>
+		public void updateConfigFile(string conf)
+		{
+			// mise à jour de la config
+			XmlDocument XmlDoc = new XmlDocument();
+			// On charge la config
+			XmlDoc.Load(AppDomain.CurrentDomain.SetupInformation.ConfigurationFile);
+			foreach (XmlElement xElement in XmlDoc.DocumentElement)
+			{
+				// on cherche LES connexions strings
+				if (xElement.Name == "connectionStrings")
+				{
+					// on initialise LE connexion string.
+					xElement.FirstChild.Attributes["connectionString"].Value = conf;
+				}
+			}
+			// On écrit le connexion string dans le fichier de config.
+			XmlDoc.Save(AppDomain.CurrentDomain.SetupInformation.ConfigurationFile);
+		}
+
+		/// <summary>
+		/// Bouton OK pour fermer paramètres.
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void buttonOK_Click(object sender, RoutedEventArgs e)
+		{
+			Close();
+		}
+	}
+}
