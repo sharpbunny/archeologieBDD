2fb51c231f70322e6c7a475912a7fb21456b16d5
Chris.D <fusion101box@gmail.com>
2017-03-27 14:00:18 +0200

    Moving class archeodata in a new file, Adding a group for the button

---
+++ InterfaceClient/ArcheoData.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceClient
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class ArcheoData
+    {
+        public ArcheoData(int line, string id, string no, string co, string dp, float lat, float lon, DateTime? ddeb, DateTime? dfin, string th)
+        {
+            LineNumber = line;
+            IDLigne = id;
+            NomSite = no;
+            NomCommune = co;
+            NomDepartement = dp;
+            Latitude = lat;
+            Longitude = lon;
+            DateDebut = ddeb;
+            DateFin = dfin;
+            Theme = th;
+        }
+
+        public int LineNumber { get; }
+
+        public string IDLigne { get; }
+
+        public string NomSite { get; set; }
+
+        public string NomCommune { get; set; }
+
+        public string NomDepartement { get; set; }
+
+        public float Latitude { get; set; }
+
+        public float Longitude { get; set; }
+
+        public DateTime? DateDebut { get; set; }
+
+        public DateTime? DateFin { get; set; }
+
+        public string Theme { get; set; }
+
+    }
+}
--- InterfaceClient/MainWindow.xaml.cs
+++ InterfaceClient/MainWindow.xaml.cs
@@ -46,47 +46,7 @@ namespace InterfaceClient
 			throw new NotSupportedException();
 		}
 
-		/// <summary>
-		/// 
-		/// </summary>
-		public class ArcheoData
-		{
-
-			public ArcheoData(int line, string id, string no, string co, string dp, float lat, float lon, DateTime? ddeb, DateTime? dfin, string th)
-			{
-				LineNumber = line;
-				IDLigne = id;
-				NomSite = no;
-				NomCommune = co;
-				NomDepartement = dp;
-				Latitude = lat;
-				Longitude = lon;
-				DateDebut = ddeb;
-				DateFin = dfin;
-				Theme = th;
-			}
-
-			public int LineNumber { get; }
-
-			public string IDLigne { get; }
-
-			public string NomSite { get; set; }
-
-			public string NomCommune { get; set; }
-
-			public string NomDepartement { get; set; }
-
-			public float Latitude { get; set; }
-
-			public float Longitude { get; set; }
-
-			public DateTime? DateDebut { get; set; }
-
-			public DateTime? DateFin { get; set; }
-
-			public string Theme { get; set; }
-
-		}
+		
 
 		/// <summary>
 		/// Permet de charger les données des sites d'interventions depuis la BDD.
